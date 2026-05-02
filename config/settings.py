d715e6edcd79800526c153a36638315416b5789c
Fatimah2804 <Fatimahkadah22@gmail.com>
2026-05-02 14:40:22 +0300

    connect cloudinary

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'core',
     'appointments',
     'gallery',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
